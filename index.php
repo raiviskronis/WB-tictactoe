f638bd912b3d477c9268bfefa6a2dad0f06bbe15
cemeris <4vitalik4@gmail.com>
2021-06-10 18:29:57 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="style.css">
+
+<div id="app" class="container">
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+
+    <a href=""></a>
+    <a href=""></a>
+    <a href=""></a>
+</div>
